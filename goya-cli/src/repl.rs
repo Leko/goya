83124626c8855c822c75e8594535481466c7d150
Leko <leko.noor@gmail.com>
2021-09-09 01:39:25 +0800

    implement double-array search

--- goya-cli/src/repl.rs
+++ goya-cli/src/repl.rs
@@ -1,5 +1,19 @@
+use morphological_analysis::double_array::DoubleArray;
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
+use std::collections::HashMap;
+
+fn demodict() -> DoubleArray {
+    // registered words: "a" and "bc"
+    let mut codes: HashMap<char, usize> = HashMap::new();
+    codes.insert('\0', 0);
+    codes.insert('a', 1);
+    codes.insert('b', 2);
+    codes.insert('c', 3);
+    let base: Vec<i32> = vec![0, 3, 0, -1, 3, 3, 7, -1];
+    let check: Vec<usize> = vec![0, 0, 0, 4, 1, 1, 5, 6];
+    return DoubleArray::new(base, check, codes);
+}
 
 pub fn start() {
     // `()` can be used when no completer is required
@@ -10,8 +24,8 @@ pub fn start() {
         match readline {
             Ok(line) => {
                 rl.add_history_entry(line.as_str());
-                morphological_analysis::extract(&line);
-                println!();
+                let result = morphological_analysis::extract(&line, &demodict());
+                println!("{:?}", result);
             }
             Err(ReadlineError::Interrupted) => break,
             Err(ReadlineError::Eof) => break,
---
+++ morphological_analysis/src/double_array.rs
@@ -0,0 +1,49 @@
+use std::collections::HashMap;
+use std::convert::TryInto;
+
+#[derive(Debug)]
+pub struct MatchResult {
+    pub next_state: usize,
+    pub can_stop: bool,
+}
+
+#[derive(Debug)]
+pub struct DoubleArray {
+    codes: HashMap<char, usize>,
+    base: Vec<i32>,
+    check: Vec<usize>,
+}
+impl DoubleArray {
+    pub fn new(base: Vec<i32>, check: Vec<usize>, codes: HashMap<char, usize>) -> DoubleArray {
+        DoubleArray { base, check, codes }
+    }
+
+    pub fn match_char(&self, s: usize, c: &char) -> Option<MatchResult> {
+        if self.base[s] < 0 {
+            return None;
+        }
+        match self.next(s, c) {
+            Some(t) => match self.check[t] == s {
+                true => Some(MatchResult {
+                    next_state: t,
+                    can_stop: self.match_char(t, &'\0').is_some(),
+                }),
+                _ => None,
+            },
+            None => None,
+        }
+    }
+
+    fn next(&self, s: usize, c: &char) -> Option<usize> {
+        match self.codes.get(c) {
+            Some(code) => {
+                let next = self.base[s] + *code as i32;
+                if next < 0 {
+                    return None;
+                }
+                Some((next).try_into().unwrap())
+            }
+            None => None,
+        }
+    }
+}
