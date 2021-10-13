cbab2fdd373506705d35e633a73376df211c3131
Leko <leko.noor@gmail.com>
2021-10-13 22:55:59 +0800

    perf: optimize features.bin
    30.2 MB -> 26.7 MB

--- goya/src/word_features.rs
+++ goya/src/word_features.rs
@@ -4,30 +4,34 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, rkyv::Archive, rkyv::Serialize, rkyv::Deserialize)]
 pub struct WordFeaturesMap {
-    index: IndexSet<String>,
+    index: Vec<String>,
     known: Vec<WordFeatures>,   // index = morpheme ID
     unknown: Vec<WordFeatures>, // index = morpheme ID
 }
 impl WordFeaturesMap {
     pub fn new(known: Vec<Vec<String>>, unknown: Vec<Vec<String>>) -> WordFeaturesMap {
-        let mut index: IndexSet<String> = IndexSet::new();
+        let mut tmp_index: IndexSet<String> = IndexSet::new();
         for features in known.iter().chain(unknown.iter()) {
             for f in features.iter() {
-                index.insert(f.to_string());
+                tmp_index.insert(f.to_string());
             }
+        }
+        let mut index = vec![String::new(); tmp_index.len()];
+        for (idx, str) in tmp_index.iter().enumerate() {
+            index[idx] = str.to_string();
         }
 
         WordFeaturesMap {
             known: known
                 .into_iter()
                 .map(|f| {
-                    WordFeatures::new(f.iter().map(|s| index.get_full(s).unwrap().0).collect())
+                    WordFeatures::new(f.iter().map(|s| tmp_index.get_full(s).unwrap().0).collect())
                 })
                 .collect(),
             unknown: unknown
                 .into_iter()
                 .map(|f| {
-                    WordFeatures::new(f.iter().map(|s| index.get_full(s).unwrap().0).collect())
+                    WordFeatures::new(f.iter().map(|s| tmp_index.get_full(s).unwrap().0).collect())
                 })
                 .collect(),
             index,
@@ -44,7 +48,7 @@ impl WordFeaturesMap {
     pub fn get_known(&self, wid: &usize) -> Option<Vec<&String>> {
         self.known.get(*wid).map(|f| {
             f.0.iter()
-                .map(|idx| self.index.get_index(*idx).unwrap())
+                .map(|idx| self.index.get(*idx).unwrap())
                 .collect()
         })
     }
@@ -52,7 +56,7 @@ impl WordFeaturesMap {
     pub fn get_unknown(&self, wid: &usize) -> Option<Vec<&String>> {
         self.unknown.get(*wid).map(|f| {
             f.0.iter()
-                .map(|idx| self.index.get_index(*idx).unwrap())
+                .map(|idx| self.index.get(*idx).unwrap())
                 .collect()
         })
     }
